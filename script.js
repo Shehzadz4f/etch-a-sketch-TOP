35ece6e8e16459a54d6d6dc9c31a432a6ca8be86
Shehzadz4f <shehzad.z4f@gmail.com>
2024-07-09 02:04:09 +0530

    Removed link to stylesheet from index.html Added event listeners to 'LIGHT TO DARK EFFECT' button such that when clicked it sets the opacity of the background of divs to 0.1 and increases by 0.1 with each click. Added event listeners to divs for random coloring of each divs which can be drawn using mouse and can be toggled using the  'RANDOM COLOR' button.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ for (let i=0; i<grid; i++) {
         box.style.minHeight = `${(500/grid)}px`;
         box.style.minWidth = `${(500/grid)}px`;
         box.style.flex = 'auto';
+        box.style.backgroundColor = 'rgba(0, 0, 0, 0)';
         container.appendChild(box); 
     }
 }
@@ -43,20 +44,22 @@ let rgbToggle = false;
 let rgbButton = document.createElement('button');
 rgbButton.classList.add('topButtons');
 rgbButton.setAttribute('id','rgbButton');
-rgbButton.textContent = 'RANDOM OFF';
+rgbButton.textContent = 'RANDOM COLOR';
 document.body.insertBefore(rgbButton, container);
 
 function randomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-    return `${r} ${g} ${b}`;
+    return `${r}, ${g}, ${b}`;
 } 
 
 rgbButton.addEventListener('click', () => {
     if(!rgbToggle) {
         rgbToggle = true;
+        lightToDarkToggle = false;
         rgbButton.textContent = 'RANDOM ON';
+        lightToDarkBtn.textContent = 'LIGHT TO DARK OFF';
     }
     else {
         rgbToggle = false;
@@ -70,34 +73,64 @@ let lightToDarkToggle = false;
 let lightToDarkBtn = document.createElement('button');
 lightToDarkBtn.classList.add('topButtons');
 lightToDarkBtn.setAttribute('id','lightToDark');
-lightToDarkBtn.textContent = 'LIGHT TO DARK EFFECT OFF';
+lightToDarkBtn.textContent = 'LIGHT TO DARK EFFECT';
 document.body.insertBefore(lightToDarkBtn, container);
 
 lightToDarkBtn.addEventListener('click', () => {
     if (!lightToDarkToggle) {
         lightToDarkToggle = true;
-        lightToDarkBtn.textContent = 'LIGHT TO DARK EFFECT ON';
+        rgbToggle = false;
+        lightToDarkBtn.textContent = 'LIGHT TO DARK ON';
+        rgbButton.textContent = 'RANDOM OFF';
     }
     else {
         lightToDarkToggle = false;
-        lightToDarkBtn.textContent = 'LIGHT TO DARK EFFECT OFF';
+        lightToDarkBtn.textContent = 'LIGHT TO DARK OFF';
     }
 });
 
-let opacity = 0;
+function lightToDarkToggleOn(currentRgb) { 
+    let RgbValues = currentRgb.match(/rgba?\((\d+),\s*(\d+),\s*(\d+),?\s*(\d*\.?\d*)?\)/);
+    //Add this rule if you want black to remain black 
+    // if (!RgbValues[4] && RgbValues[4] === 'rgb(0, 0, 0)') {
+    //     return 'rgba (0, 0, 0, 1)'; }
+
+    //If aplha doesnt exist then set alpha to 0.1
+    //Remove this rule if you want colored boxes to remain dark
+    if (!RgbValues[4]) {
+        return `rgba(${RgbValues[1]}, ${RgbValues[2]}, ${RgbValues[3]}, 0.1)`;
+    }  
+    else if (RgbValues[4]) {
+        //if alpha is less than 1 then increment by 0.1
+        let alpha = Number(RgbValues[4]) + 0.1;   
+        if (alpha <= 1) {
+            return `rgba(${RgbValues[1]}, ${RgbValues[2]}, ${RgbValues[3]}, ${alpha})`;
+        }
+    } 
+}
+
+let boxes = document.querySelectorAll('.boxes');
+
 let previousElementColor;
 let currentElementColor;
 
-let boxes = document.querySelectorAll('.boxes');
 boxes.forEach((element) => {
     element.addEventListener('mouseenter', (event) => {
-        currentElementColor = `rgb(${randomColor()})`;
+        currentElementColor = `rgba(${randomColor()})`;
         if(isMouseDown && !rgbToggle) {
-            event.target.style.backgroundColor = "black";
+            if(lightToDarkToggle) {
+                event.target.style.backgroundColor = lightToDarkToggleOn(event.target.style.backgroundColor);
+            } else {
+                event.target.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+            }
         }
         else if(isMouseDown && rgbToggle) {
-            event.target.style.backgroundColor = currentElementColor;
-            previousElementColor = currentElementColor;
+            if(lightToDarkToggle) {
+                event.target.style.backgroundColor = lightToDarkToggleOn(event.target.style.backgroundColor);
+            } else {
+                event.target.style.backgroundColor = currentElementColor;
+                previousElementColor = currentElementColor;
+            }
         }
     });
 });
@@ -105,21 +138,31 @@ boxes.forEach((element) => {
 boxes.forEach((element) => {
     element.addEventListener('mouseleave', (event) => {
         if(isMouseDown && !rgbToggle) {
-            event.target.style.backgroundColor = "black";
-        }
-        else if(isMouseDown && rgbToggle) {
-            event.target.style.backgroundColor = previousElementColor;
+            if(isMouseDown && !rgbToggle) {
+                if(lightToDarkToggle) {
+                    event.target.style.backgroundColor = lightToDarkToggleOn(event.target.style.backgroundColor);
+                } else {
+                    event.target.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+                }
+            } else if (isMouseDown && rgbToggle) {
+                if(lightToDarkToggle) {
+                    event.target.style.backgroundColor = lightToDarkToggleOn(event.target.style.backgroundColor);
+                } else {
+                    event.target.style.backgroundColor = previousElementColor;
+                }
+            }
         }
     });
 });
 
 boxes.forEach((element) => {
     element.addEventListener('click', (event) => {
-        if(!rgbToggle) {
-            event.target.style.backgroundColor = "black";
-        }
-        else if(rgbToggle) {
-            event.target.style.backgroundColor = `rgb(${randomColor()})`;
+        if(!rgbToggle && lightToDarkToggle) {
+            event.target.style.backgroundColor = lightToDarkToggleOn(event.target.style.backgroundColor);
+        } else if (!rgbToggle && !lightToDarkToggle) {
+            event.target.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+        } else if(rgbToggle) {
+                event.target.style.backgroundColor = `rgba(${randomColor()})`;
         }
     });
 });
@@ -136,7 +179,3 @@ resetButton.addEventListener('click', () => {
         box.style.backgroundColor = "white";
     }
 });
-
-
-
-
